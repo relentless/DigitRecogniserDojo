053a7f76a75a27ac709d02cea254dab2ea6cd81b
alexdgarland <alex.d.garland@gmail.com>
2014-04-27 15:30:48 +0100

    Complete Visualiser

--- Python Base Solution/Code/Visualiser.py
+++ Python Base Solution/Code/Visualiser.py
@@ -23,7 +23,7 @@ except ImportError as e:
 
 def Draw (text, pixels):
     """Function to draw the pixel data passed to it.
-    TK Inter graphic library is used, simply because this is almost always available
+    TK Inter graphic widget library is used, simply because this is almost always available
     where any version of Python is installed.    
     Something like Python Image Library (PIL) would be more efficient but less available.
     """
@@ -39,19 +39,41 @@ def Draw (text, pixels):
         root = Tk()
         root.title(text)
 
-        frame = Frame(root, width=500, height=500)
-        frame.pack()
-        
+        tilesize = 10
+        charactersize = 28
+
+        def dimension():
+            return tilesize * charactersize
+
+        mainframe = Frame(root, width=dimension(), height=dimension())
+        mainframe.pack()
+
+
+        for pixel in enumerate(pixels):
+
+            xOffset = (pixel[0] % charactersize) * tilesize
+            yOffset = (pixel[0] / charactersize) * tilesize
+            colour = getmonochromehex(pixel[1])
+            
+            # Draw each pixel as an individual frame widget (!)
+            pixeltile = Frame(mainframe, width=tilesize, height = tilesize, background = colour)
+            pixeltile.place( x = xOffset, y = yOffset )
+            
         root.mainloop()
 
 
+def getmonochromehex(pixelvalue):
+    partialhex = hex(pixelvalue).replace("0x", "")
+    if len(partialhex)== 1:
+        partialhex = "0" + partialhex
+    return "#{0}{0}{0}".format(partialhex)
 
 
 def Test():
+    
+    testpixels = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,220,179,6,0,0,0,0,0,0,0,0,9,77,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,28,247,17,0,0,0,0,0,0,0,0,27,202,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,242,155,0,0,0,0,0,0,0,0,27,254,63,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,160,207,6,0,0,0,0,0,0,0,27,254,65,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,127,254,21,0,0,0,0,0,0,0,20,239,65,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,77,254,21,0,0,0,0,0,0,0,0,195,65,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,70,254,21,0,0,0,0,0,0,0,0,195,142,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,56,251,21,0,0,0,0,0,0,0,0,195,227,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,222,153,5,0,0,0,0,0,0,0,120,240,13,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,67,251,40,0,0,0,0,0,0,0,94,255,69,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,234,184,0,0,0,0,0,0,0,19,245,69,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,234,169,0,0,0,0,0,0,0,3,199,182,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,154,205,4,0,0,26,72,128,203,208,254,254,131,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,61,254,129,113,186,245,251,189,75,56,136,254,73,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,15,216,233,233,159,104,52,0,0,0,38,254,73,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,18,254,73,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,18,254,73,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,206,106,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,186,159,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,6,209,101,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-    print()
-    Draw('Hello world, this is a test', 0)
-
+    Draw('Testing - expected number is 4', testpixels)
 
 if __name__ == '__main__':
     Test()
