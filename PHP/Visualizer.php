1015e5fd55922a16808f2fd39f956ca72081b590
Alfonsas Cirtautas <alfonsas.cirtautas@gmail.com>
2014-05-07 01:21:02 +0300

    Added simple visualiser using pseudo character

--- PHP/Visualizer.php
+++ PHP/Visualizer.php
@@ -6,7 +6,20 @@ class Visualiser
      * @param string $text
      * @param array $pixels
      */
-    public static function Draw( $text, $pixels) {
-        // TODO: implement Draw
+    public static function Draw($text, $pixels)
+    {
+        $rc = ceil(sqrt(count($pixels)));
+        $dt = [' ', '░', '▒', '▓', '█'];
+        $dc = count($dt);
+
+        echo PHP_EOL, "[ $text ]", PHP_EOL;
+        echo '╔', str_repeat('══', $rc), '╗';
+        foreach ($pixels as $i => $px) {
+            if ($i % $rc == 0) {
+                echo $i ? '║' : '', PHP_EOL, '║';
+            }
+            echo str_repeat($dt[$px % $dc], 2);
+        }
+        echo '║', PHP_EOL, '╚', str_repeat('══', $rc), '╝', PHP_EOL;
     }
 }
