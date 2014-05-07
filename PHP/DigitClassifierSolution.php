608d8be6dac07e85ef8dc66e1ccfeefd2080a1ff
Alfonsas Cirtautas <alfonsas.cirtautas@gmail.com>
2014-05-07 10:30:13 +0300

    Added sample solution

---
+++ PHP/DigitClassifierSolution.php
@@ -0,0 +1,114 @@
+<?php
+
+require 'Visualizer.php';
+
+// 1. READING THE DATA
+
+$dataLines = file(__DIR__ . '/trainingsample.csv');
+
+// 2. CLEANING UP HEADERS
+
+$dataNoHeader = array_slice($dataLines, 1);
+
+// 3. EXTRACTING COLUMNS
+
+$dataValues = array_map(
+    function ($dataLine) {
+        return explode(',', $dataLine);
+    },
+    $dataNoHeader
+);
+
+// 4. CONVERTING FROM STRINGS TO INTS
+
+$dataNumbers = array_map(
+    function ($stringArray) {
+        return array_map('intval', $stringArray);
+    },
+    $dataValues
+);
+
+// 5. CONVERTING ARRAYS TO RECORDS
+
+class DigitalRecord
+{
+    public $Label;
+    public $Pixels = [];
+
+    public function __construct($Label, $Pixels)
+    {
+        $this->Label  = $Label;
+        $this->Pixels = $Pixels;
+    }
+}
+
+$dataRecords = array_map(
+    function ($numberRow) {
+        return new DigitalRecord(array_shift($numberRow), $numberRow);
+    },
+    $dataNumbers
+);
+
+// 6. LET'S SEE SOME DIGITS!
+
+foreach (array_slice($dataRecords, 0, 3) as $record) {
+    Visualiser::Draw($record->Label, $record->Pixels);
+}
+
+// 7. TRAINING vs VALIDATION DATA
+
+$trainingData   = array_slice($dataRecords, 0, 1600);
+$validationData = array_slice($dataRecords, 1600);
+
+// 8. COMPUTING DISTANCES
+
+function calculateDistance($testDigit, $knownDigit)
+{
+    return abs($testDigit - $knownDigit);
+}
+
+// 9. WRITING THE CLASSIFIER FUNCTION
+
+function classify($trainingData, $unknownPixels)
+{
+    $neighbors = [];
+    foreach ($trainingData as $i => $test) {
+        $distances = [];
+        foreach ($test->Pixels as $j => $pixel) {
+            $distances[] = calculateDistance($unknownPixels[$j], $pixel);
+        }
+        $neighbors[$i] = array_sum($distances);
+    }
+
+    asort($neighbors);
+
+    return $trainingData[key($neighbors)]->Label;
+}
+
+// 10. SEE THE CLASSIFIER IN ACTION
+
+$predicted = classify($trainingData, $validationData[0]->Pixels);
+
+
+// 11. EVALUATING THE MODEL AGAINST VALIDATION DATA
+
+$processed = $matched = $accuracy = 0;
+foreach ($validationData as $i => $record) {
+    $processed++;
+
+    $actual    = $record->Label;
+    $predicted = classify($trainingData, $record->Pixels);
+
+    if ($actual == $predicted) {
+        $matched++;
+    }
+
+    $accuracy = round(($matched / $processed) * 100, 2);
+
+
+    //Visualiser::Draw("$predicted == $actual ?" , $record->Pixels);
+    echo "Actual: $actual,  Predicted: $predicted, Accuracy: $accuracy %", PHP_EOL;
+}
+
+// 12. NEXT STEPS
+
