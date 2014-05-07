ad29e4bca7afd456dff2d2ceb423b152d814faf4
Alfonsas Cirtautas <alfonsas.cirtautas@gmail.com>
2014-05-07 15:13:35 +0300

    Changed Visualiser to not use utf8 characters

--- PHP/Visualizer.php
+++ PHP/Visualizer.php
@@ -9,17 +9,17 @@ class Visualiser
     public static function Draw($text, $pixels)
     {
         $rc = ceil(sqrt(count($pixels)));
-        $dt = [' ', '░', '▒', '▓', '█'];
+        $dt = [' ', '.','"', '*',':','%','#','X'];
         $dc = count($dt);
 
         echo PHP_EOL, "[ $text ]", PHP_EOL;
-        echo '╔', str_repeat('══', $rc), '╗';
+        echo '+', str_repeat('--', $rc), '+';
         foreach ($pixels as $i => $px) {
             if ($i % $rc == 0) {
-                echo $i ? '║' : '', PHP_EOL, '║';
+                echo $i ? '|' : '', PHP_EOL, '|';
             }
             echo str_repeat($dt[$px % $dc], 2);
         }
-        echo '║', PHP_EOL, '╚', str_repeat('══', $rc), '╝', PHP_EOL;
+        echo '|', PHP_EOL, '+', str_repeat('--', $rc), '+', PHP_EOL;
     }
 }
